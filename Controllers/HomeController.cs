713d9fdb883a37b4bdb69855d9663bfef49ec6ed
Harrison <47501747+Code-acus@users.noreply.github.com>
2023-04-29 13:17:47 -0600

    New action method added to HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,5 +22,10 @@ namespace PartyInvites_v2
             Repository.AddResponse(guestResponse);
             return View("Thanks", guestResponse);
         }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
     }
 }
